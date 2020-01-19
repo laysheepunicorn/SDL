3cda23efb0e82edd3d9ca38e05f409317b697006
Lay1007 <41284343+laysheepunicorn@users.noreply.github.com>
2020-01-19 16:11:55 +0800

    Add files via upload

---
+++ 实验六 DLL注入/code/injectAllTheThings/auxiliary.h
@@ -0,0 +1,8 @@
+#pragma once
+/*
+* definitions for auxiliary functions
+*/
+DWORD findPidByName(wchar_t * pname);
+VOID displayHelp();
+DWORD getThreadID(DWORD pid);
+BOOL SetSePrivilege();
---
+++ 实验六 DLL注入/code/injectAllTheThings/fheaders.h
@@ -0,0 +1,47 @@
+#pragma once
+// Technique 1: CreateRemoteThread
+DWORD demoCreateRemoteThreadW(PCWSTR pszLibFile, DWORD dwProcessId);
+
+// Technique 2: NtCreateThreadEx
+
+struct NtCreateThreadExBuffer {
+	ULONG Size;
+	ULONG Unknown1;
+	ULONG Unknown2;
+	PULONG Unknown3;
+	ULONG Unknown4;
+	ULONG Unknown5;
+	ULONG Unknown6;
+	PULONG Unknown7;
+	ULONG Unknown8;
+};
+
+typedef NTSTATUS(WINAPI *LPFUN_NtCreateThreadEx) (
+	PHANDLE hThread,
+	ACCESS_MASK DesiredAccess,
+	LPVOID ObjectAttributes,
+	HANDLE ProcessHandle,
+	LPTHREAD_START_ROUTINE lpStartAddress,
+	LPVOID lpParameter,
+	BOOL CreateSuspended,
+	ULONG StackZeroBits,
+	ULONG SizeOfStackCommit,
+	ULONG SizeOfStackReserve,
+	LPVOID lpBytesBuffer
+	);
+
+
+
+// Function Pointer Typedef for RtlCreateUserThread
+typedef DWORD(WINAPI * pRtlCreateUserThread)(
+	IN HANDLE 					ProcessHandle,
+	IN PSECURITY_DESCRIPTOR 	SecurityDescriptor,
+	IN BOOL 					CreateSuspended,
+	IN ULONG					StackZeroBits,
+	IN OUT PULONG				StackReserved,
+	IN OUT PULONG				StackCommit,
+	IN LPVOID					StartAddress,
+	IN LPVOID					StartParameter,
+	OUT HANDLE 					ThreadHandle,
+	OUT LPVOID					ClientID
+	);
